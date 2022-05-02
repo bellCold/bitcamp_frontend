2827d94bce072f12a657d0fe460efc7d146be854
KimJongChan <102959382+bellCold@users.noreply.github.com>
2022-05-02 20:15:03 +0900

    Add files via upload

---
+++ 2022_05_02/java.js
@@ -0,0 +1,12 @@
+function loginPage()  {
+    location.href = 'youtube.html'
+  }
+  
+function dodisplay() {
+  var con =document.getElementById("btn");
+  if(con.style.display=='none') {
+    con.style.display = 'block';
+  }else {
+    con.style.display = 'none';
+  }
+}
